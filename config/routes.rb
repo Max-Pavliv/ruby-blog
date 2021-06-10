cf49546f582599ac5bda342c7d96feff20cf3f60
Max-Pavliv <84768891+Max-Pavliv@users.noreply.github.com>
2021-06-10 14:56:52 +0300

    Added: Styels

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  get "create" => 'articles#new'
+
   resources :articles do
-    resources :comments
+   resources :comments
   end
 end
